779afc9b76d545ab2dfbf86d03bfc2af7499179c
nicolofp <nicolo.fp@gmail.com>
2023-11-09 22:32:56 -0500

    add changes

--- J_DuckDB.jl
+++ J_DuckDB.jl
@@ -1,13 +1,15 @@
 using DuckDB
 using DataFrames
 
-con = DBInterface.connect(DuckDB.DB)
+con = DBInterface.connect(DuckDB.DB, ":memory:")
 
-# create a DataFrame
-df = DataFrame(a = [1, 2, 3], b = [42, 84, 42])
+@time x = DataFrame(DBInterface.execute(con,
+           """
+           SELECT *
+           FROM 'C:/Users/nicol/Documents/Datasets/Air_quality/waqi-covid19-airqualitydata-2023.csv'
+           """));
 
-# register it as a view in the database
-DuckDB.register_data_frame(con, df, "my_df")
 
-# run a SQL query over the DataFrame
-results = DBInterface.execute(con, "SELECT * FROM my_df")
+
+
+
