f96e88677a5d8a95bb5e65b9ff66c285f46f3012
nicolofp <nicolo.fp@gmail.com>
2023-11-14 20:03:58 -0500

    add rna_seq pipeline

--- J_DuckDB.jl
+++ J_DuckDB.jl
@@ -1,15 +1,27 @@
 using DuckDB
 using DataFrames
+using CSV
 
 con = DBInterface.connect(DuckDB.DB, ":memory:")
-
+#@time y = CSV.read("C:/Users/nicol/Downloads/Bioinformatics/elegant/SRR12280931.fastq.gz-aligned/abundance.tsv", DataFrame, delim = '\t')
 @time x = DataFrame(DBInterface.execute(con,
            """
            SELECT *
-           FROM 'C:/Users/nicol/Documents/Datasets/Air_quality/waqi-covid19-airqualitydata-2023.csv'
+           FROM 'C:/Users/nicol/Downloads/Bioinformatics/elegant/SRR12280931.fastq.gz-aligned-2/abundance.tsv'
+           """));
+x.target_id = string.(x.target_id)
+for i in 1:size(x,1) 
+    x.target_id[i] = split(x.target_id[i,1],"|")[1] 
+end
+
+# 
+
+@time y = DataFrame(DBInterface.execute(con,
+           """
+           SELECT *
+           FROM 'C:/Users/nicol/Downloads/Bioinformatics/mapping.csv'
            """));
 
-
-
+DT = innerjoin(x, y, on = :target_id => :column0)
 
 
