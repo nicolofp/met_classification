29ec193456b1072481b34ba4c1581de14b579751
nicolofp <nicolo.fp@gmail.com>
2023-12-18 15:48:41 -0500

    define minor changes

--- J_DuckDB.jl
+++ J_DuckDB.jl
@@ -1,6 +1,11 @@
 using DuckDB
 using DataFrames
-using Arrow
+using Arrow, HTTP
+
+
+# https://envbran.s3.us-east-2.amazonaws.com/methylation/table1.arrow
+df = DataFrame(Arrow.Table(download("https://envbran.s3.us-east-2.amazonaws" * 
+                            ".com/methylation/table1.arrow")));
 
 con = DBInterface.connect(DuckDB.DB, ":memory:")
 #@time y = CSV.read("C:/Users/nicol/Downloads/Bioinformatics/elegant/SRR12280931.fastq.gz-aligned/abundance.tsv", DataFrame, delim = '\t')
@@ -19,7 +24,7 @@ end
 @time y = DataFrame(DBInterface.execute(con,
            """
            SELECT *
-           FROM 'C:/Users/nicol/Downloads/Bioinformatics/GSE133758_globinblock_rawcounts.csv.gz'
+           FROM 'https://envbran.s3.us-east-2.amazonaws.com/methylation/table1.arrow'
            """));
 
 DT = innerjoin(x, y, on = :target_id => :column0)
@@ -30,3 +35,11 @@ con = DBInterface.connect(DuckDB.DB, ":memory:")
            SELECT *
            FROM 'C:/Users/nicol/Downloads/Bioinformatics/elegant/SRR12280931.fastq.gz-aligned-2/abundance.tsv'
            """));
+
+mean = [2.,3.]
+C = [0.2 0; 0 0.3]
+d = MvNormal(mean, C)
+x = rand(d, 2000)
+
+td = MvTDist(5,mean, C)
+y = rand(td,2000)
--- test_mixture.jl
+++ test_mixture.jl
@@ -8,14 +8,14 @@ Turing.setrdcache(true)
 # Mixture Test
 rng = MersenneTwister(1990);
 N = 342
-XM = rand(rng, Normal(0.0,0.2),N,5)
+XM = rand(rng, Normal(0.0,0.2),N,55)
 XC = rand(rng, Normal(0.0,0.2),N,2)
-w = vec([0.5 0.2 0.2 0.05 0.05])
+w = rand(Dirichlet(ones(55)))#vec([0.5 0.2 0.2 0.05 0.05])
 a = 1.1
 b = -0.23
 d = vec([1.1 -0.12])
 
-for i in 1:5
+for i in 1:size(XM,2)
     XM[:,i] = ecdf(XM[:,i])(XM[:,i])*4
 end
 
@@ -53,6 +53,11 @@ function DirichletLogit(μ, ϕ)
     return Dirichlet(α)
 end
 
+# k = 20.0
+# a = 10*[1,1,1,1,1]#rand(Gamma(k,k),5)
+# b = rand(Gamma(2.0,2.0))
+# rand(DirichletLogit(a,b))
+
 @model function bwqs_soft(cx, mx, y)
     
     # Set variance prior.
@@ -66,8 +71,8 @@ end
 
     # Set the priors on our coefficients.
     nfeatures = size(mx, 2)
-    alpha ~ filldist(Gamma(1.0,1.0), nfeatures)
-    phi ~ Gamma(1.0,1.0)
+    alpha = 50*ones(nfeatures) #~ filldist(Gamma(1.0,1.0), nfeatures)
+    phi = 10 #~ Gamma(2.0,2.0)
     w ~ DirichletLogit(alpha, phi) 
     #w = softmax(alpha)
 
@@ -100,7 +105,8 @@ end
 mx1 = bwqs_soft(XC, XM, y)
 mx2 = bwqs_new(XC, XM, y)
 mx3 = bwqs(XC, XM, y)
-chain_mx1 = sample(mx1, NUTS(), 5000);#, thinning=10, discard_initial=2000);
+chain_mx1 = sample(mx1, NUTS(), 1000);#, thinning=10, discard_initial=2000);
+#chain_mx1 = sample(mx1, NUTS(), MCMCThreads(), 2000, 4)
 chain_mx2 = sample(mx2, NUTS(), 1000);
 chain_mx3 = sample(mx3, NUTS(), 1000);
 
